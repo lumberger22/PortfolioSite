c0869f2a9927008865be41479cfd37f0e6a3e5f5
lucasvt22 <lucas22@vt.edu>
2024-03-26 16:22:06 -0400

    Create basic structure of About Me section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,18 @@
+export default function AboutMe() {
+
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="About Me"/>
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills-section--heading">About Me</h1>
+                    <p className="hero--section-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit temporibus necessitatibus ea iure autem labore! Commodi harum fugiat facere, numquam maxime distinctio? Error quasi iste perspiciatis ipsa nobis animi pariatur?</p>
+                    <p className="hero--section-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit temporibus necessitatibus ea iure autem labore!</p>
+                </div>
+            </div>
+        </section>
+    )
+}
