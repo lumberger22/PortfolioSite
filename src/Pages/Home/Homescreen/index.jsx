7c787f957d847aae80e72026edf85d8a58995551
lucasvt22 <lucas22@vt.edu>
2024-03-26 17:00:43 -0400

    Add MyPortfolio component to index

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,13 +1,15 @@
 import AboutMe from "../AboutMe";
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
+import MyPortfolio from "../MyPortfolio";
 
 export default function Home() {
     return (
         <>
-            <HeroSection/>
-            <MySkills/>
-            <AboutMe/>
+            <HeroSection />
+            <MySkills />
+            <AboutMe />
+            <MyPortfolio />
         </>
     );
 }
