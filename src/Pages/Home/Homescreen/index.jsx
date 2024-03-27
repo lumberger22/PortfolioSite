ab2ef3276c70b525a33282079aa56107323817ad
lucasvt22 <lucas22@vt.edu>
2024-03-26 21:34:13 -0400

    Add components to homescreen

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -2,6 +2,8 @@ import AboutMe from "../AboutMe";
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
 import MyPortfolio from "../MyPortfolio";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
 
 export default function Home() {
     return (
@@ -10,6 +12,8 @@ export default function Home() {
             <MySkills />
             <AboutMe />
             <MyPortfolio />
+            <ContactMe />
+            <Footer />
         </>
     );
 }
