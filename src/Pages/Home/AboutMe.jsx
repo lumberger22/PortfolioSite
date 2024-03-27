ddafe4b8e0b8d96ca495a7f6759b502998614898
lucasvt22 <lucas22@vt.edu>
2024-03-26 23:40:19 -0400

    Add button to view resume in new tab

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -10,7 +10,9 @@ export default function AboutMe() {
                     <p className="section--title">About</p>
                     <h1 className="skills-section--heading">About Me</h1>
                     <p className="hero--section-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit temporibus necessitatibus ea iure autem labore! Commodi harum fugiat facere, numquam maxime distinctio? Error quasi iste perspiciatis ipsa nobis animi pariatur?</p>
-                    <p className="hero--section-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit temporibus necessitatibus ea iure autem labore!</p>
+                    <a href="./LucasUmbergerResume24.pdf" target="_blank" rel="noopener noreferrer">
+                        <button className="btn btn-primary">View Resume</button>  
+                    </a>
                 </div>
             </div>
         </section>
