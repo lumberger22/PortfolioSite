0843e0a39bcc17bbcb2007c80b52e639061dc09f
lucasvt22 <lucas22@vt.edu>
2024-03-27 19:10:07 -0400

    Add Skills tab to navbar and link to MySkills section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -5,9 +5,9 @@ export default function HeroSection() {
                 <div className="hero--section--content">
                     <p className="section--title">Hey, I'm Lucas</p>
                     <h1 className="hero--section--title">
-                        <span className="hero--section-title--color">Full Stack</span>{" "}
+                        <span className="hero--section-title--color">Software</span>{" "}
                         <br />
-                        Developer
+                        Engineer
                     </h1>
                     <p className="hero--section-description">
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit.
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -67,6 +67,20 @@ function Navbar() {
                             smooth={true}
                             offset={-70}
                             duration={500}
+                            to="mySkills"
+                            className="navbar--content"
+                        >
+                                Skills
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            onClick={closeMenu} 
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
                             to="AboutMe"
                             className="navbar--content"
                         >
