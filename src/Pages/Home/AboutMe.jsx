ea3e2986d0c444c12cc10698c169678ec15194a3
lucasvt22 <lucas22@vt.edu>
2024-04-08 22:47:16 -0400

    Add short desciption for about me

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
                 <div className="hero--section--content">
                     <p className="section--title">About</p>
                     <h1 className="skills-section--heading">About Me</h1>
-                    <p className="hero--section-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit temporibus necessitatibus ea iure autem labore! Commodi harum fugiat facere, numquam maxime distinctio? Error quasi iste perspiciatis ipsa nobis animi pariatur?</p>
+                    <p className="hero--section-description">I'm a Computer Science sophomore at Virginia Tech, passionate about merging technology with creativity. Known for my adaptability and visionary approach, I excel in solving complex problems. My skills span leadership, detail orientation, effective communication, strategic thinking, and teamwork.</p>
                     <a href="./LucasUmbergerResume24.pdf" target="_blank" rel="noopener noreferrer">
                         <button className="btn btn-primary">View Resume</button>  
                     </a>
